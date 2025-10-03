803c5178243fcedadc2842a0dbbccb957bd6a76f
SiRipo92 <ripochesierra@gmail.com>
2025-10-03 12:39:59 +0200

    Backend feat: calls for poster images to improve efficiency and efforts to recover a working poster image

--- backend/api/v1/titles/urls.py
+++ backend/api/v1/titles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import MovieTitleListView, MovieTitleDetailView
+from .posters import poster_proxy  # new to prevent console errors and to check image_urls
 
 urlpatterns = [
     path('', MovieTitleListView.as_view(), name="movie-list"),
     path('<int:pk>', MovieTitleDetailView.as_view(), name="movie-detail"),
+    path('posters/', poster_proxy, name="poster-proxy"),
 ]
